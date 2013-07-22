4534737278b8c7313eec266d3936c612bebd9cc4
Jonathan Roa <jroa3000@gmail.com>
2013-07-22 16:14:05 -0500

    Demo Web MVC 3

---
+++ DemoWeb/DemoWeb/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWeb.Models;
+
+namespace DemoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(string id)
+        {
+            var modelo = new ModeloDemo();
+            //ViewBag.Usuario = id;
+            modelo.Nombre = id;
+            modelo.Apellido = "otro valor";
+            return View(modelo);
+        }
+
+        public ActionResult Index2() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index2(ModeloDemo modelo) {
+            if (!ModelState.IsValid) {
+                return View(modelo);
+            }
+
+            return View("VistaConDatos", modelo);
+        }
+
+    }
+}
